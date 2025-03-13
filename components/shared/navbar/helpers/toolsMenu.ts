603ecf0e4ab055d68ddd939deb09cc582dd1f152
test <nexiloxdev@gmail.com>
2025-03-12 21:27:49 -0300

    tools menu

---
+++ components/shared/navbar/helpers/toolsMenu.ts
@@ -0,0 +1,8 @@
+export const menuItems = [
+  { icon: 'https://i.imgur.com/kuaBMr3.png', label: 'Cursos', route: '/cursos' },
+  { icon: 'https://i.imgur.com/Y6T1ctb.png', label: 'Usuarios', route: '/usuarios' },
+  { icon: 'https://i.imgur.com/TSxxmZF.png', label: 'Ventas', route: '/ventas' },
+  { icon: 'https://i.imgur.com/sf4QSy6.png', label: 'Empresas', route: '/empresas' },
+  { icon: 'https://i.imgur.com/BfKrsvl.png', label: 'Contacto', route: '/contacto' },
+  { icon: 'https://i.imgur.com/awfTxzg.png', label: 'Notificaciones', route: '/notificaciones' },
+];
