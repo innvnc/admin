f20523bf298d703e053038503dac1baf3a0c9e08
Support <argserverdev@gmail.com>
2025-03-15 20:23:18 -0300

    Styles courses list

--- components/courses/CoursesList.tsx
+++ components/courses/CoursesList.tsx
@@ -3,35 +3,46 @@ import { UI } from '../shared';
 import { CoursesTable } from './CoursesTable';
 import { getCourseMenuOptions } from './helpers';
 
+
 export const CoursesList = () => {
+
   const courseMenuOptions = getCourseMenuOptions();
 
   return (
-    <div className="flex flex-col px-4">
-      <div className="flex w-full flex-col">
+    <div className="courses-list">
+      <div className="courses-list__container">
 
         <UI.Tabs aria-label="Options" isVertical>
-          { courseMenuOptions.map( ( option ) => (
-            <UI.Tab
-              key={ option.key }
-              title={
-                <div className="flex items-center space-x-3">
-                  <span className="flex items-center justify-center">
-                    { option.icon }
-                  </span>
-                  <span>{ option.label }</span>
-                </div>
-              }
-            >
-              <UI.Card>
-                <UI.CardBody>
-                  <CoursesTable courseStatus={ option.status } />
-                </UI.CardBody>
-              </UI.Card>
-            </UI.Tab>
-          ) ) }
+          {
+            courseMenuOptions.map( ( option ) => (
+              <UI.Tab
+                key={ option.key }
+                title={
+                  <div className="courses-list__tab-title">
+
+                    <span className="courses-list__icon">
+                      { option.icon }
+                    </span>
+
+                    <span className="courses-list__label">
+                      { option.label }
+                    </span>
+
+                  </div>
+                }
+              >
+                <UI.Card>
+                  <UI.CardBody>
+                    <CoursesTable courseStatus={ option.status } />
+                  </UI.CardBody>
+                </UI.Card>
+              </UI.Tab>
+            ) )
+          }
         </UI.Tabs>
+
       </div>
     </div>
   );
+
 };
