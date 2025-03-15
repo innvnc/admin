60fdafad54028cdc68ccd1a94beda8af65370626
Support <argserverdev@gmail.com>
2025-03-15 20:18:00 -0300

    courses list

--- components/courses/CoursesList.tsx
+++ components/courses/CoursesList.tsx
@@ -1,39 +1,17 @@
 'use client';
-
 import { UI } from '../shared';
-import { Icons } from '../shared/ui';
 import { CoursesTable } from './CoursesTable';
-
-type CourseStatus = "published" | "hidden" | "deleted";
+import { getCourseMenuOptions } from './helpers';
 
 export const CoursesList = () => {
-  const tabOptions = [
-    {
-      key: "publicados",
-      icon: <Icons.IoEyeOutline size={ 20 } />,
-      label: "Publicados",
-      status: 'published' as CourseStatus
-    },
-    {
-      key: "ocultos",
-      icon: <Icons.IoEyeOffOutline size={ 20 } />,
-      label: "Eliminados",
-      status: 'hidden' as CourseStatus
-    },
-    {
-      key: "eliminados",
-      icon: <Icons.IoTrashOutline size={ 20 } />,
-      label: "Eliminados",
-      status: 'deleted' as CourseStatus
-    }
-  ];
+  const courseMenuOptions = getCourseMenuOptions();
 
   return (
     <div className="flex flex-col px-4">
       <div className="flex w-full flex-col">
 
         <UI.Tabs aria-label="Options" isVertical>
-          { tabOptions.map( ( option ) => (
+          { courseMenuOptions.map( ( option ) => (
             <UI.Tab
               key={ option.key }
               title={
