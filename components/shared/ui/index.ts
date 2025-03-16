161a66104639ca25cdcb1ef913901589703f7421
Support <argserverdev@gmail.com>
2025-03-16 13:14:32 -0300

    generic table

--- components/shared/ui/index.ts
+++ components/shared/ui/index.ts
@@ -2,4 +2,5 @@ export * as Icons from './ion-icons';
 export * as UI from './nextui-components';
 export * from './icons';
 export * from './primitives';
+export * from './table';
 export * from './ThemeSwitch';
