42eea5b28ac3605c5c7851e364e97f88dcfbf026
support <argserverdev@gmail.com>
2025-04-01 10:52:14 -0300

    custom toast example

--- components/shared/ui/index.ts
+++ components/shared/ui/index.ts
@@ -1,5 +1,6 @@
 export * as Icons from './ion-icons';
 export * as UI from './nextui-components';
+export * from './custom-toast/CustomToast';
 export * from './icons';
 export * from './primitives';
 export * from './table';
